cc952594f3a787da62a1607bafa714ebdbd98c01
Zülal Nebin <zllnbn1997@gmail.com>
2021-09-19 22:46:22 +0300

    updated

--- reactjs-projects/odev1/src/lib/service.js
+++ reactjs-projects/odev1/src/lib/service.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 
-export default async function getData(user_id) {
+async function getData(user_id) {
 
-    const { data: user } = await axios("https://jsonplaceholder.typicode.com/users/" + user_id);
+    const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`);
 
-    const { data: post } = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + user_id);
+    const { data: post } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
 
     console.log("user: ", user);
     console.log("posts: ", post);
@@ -13,3 +13,5 @@ export default async function getData(user_id) {
     // return `user: ${user}
     // posts: ${post}`;
 }
+
+export default getData;
