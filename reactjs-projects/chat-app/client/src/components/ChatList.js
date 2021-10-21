ff90d3ef3fa6aa6d26d84aeba7d0594e936dacd0
Zülal Nebin <zllnbn1997@gmail.com>
2021-10-22 00:13:56 +0300

    update

---
+++ reactjs-projects/chat-app/client/src/components/ChatList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from "./styles.module.css";
+
+import { useChat } from "../context/ChatContext";
+import ChatItem from './ChatItem';
+
+import ScrollableFeed from 'react-scrollable-feed'
+
+function ChatList() {
+    const { messages } = useChat();
+
+    // console.log(data);
+    return (
+        <div className={styles.chatlist}>
+            <ScrollableFeed forceScroll={true} >
+                {messages.map((item, key) => (
+                    <ChatItem key={key} item={item} />
+                ))}
+            </ScrollableFeed>
+        </div>
+    );
+}
+
+export default ChatList;
