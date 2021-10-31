825b7e55131d6cb9dd0fedbef9e524aa76cbfe57
Zülal Nebin <zllnbn1997@gmail.com>
2021-10-31 17:53:33 +0300

    add veri yapıları

--- reactjs-projects/odev1/src/lib/service.js
+++ reactjs-projects/odev1/src/lib/service.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-
 async function getData(user_id) {
+  const apiUrl = "https://jsonplaceholder.typicode.com";
 
-    const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`);
+  const { data: user } = await axios(`${apiUrl}/users/${user_id}`);
 
-    const { data: post } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
+  const { data: post } = await axios(`${apiUrl}/posts?userId=${user_id}`);
 
-    console.log("user: ", user);
-    console.log("posts: ", post);
+  console.log("user: ", user);
+  console.log("posts: ", post);
 
-    // return `user: ${user}
-    // posts: ${post}`;
+  // return `user: ${user}
+  // posts: ${post}`;
 }
 
 export default getData;
